e2164d5988bc1192123990b3717d7f4063ce3c6c
Nikolas Ferenčák <experimentproduction@gmail.com>
2020-01-28 12:46:42 +0100

    jhkjnk

--- src/app/sites/demandware/yeezysupply.service.ts
+++ src/app/sites/demandware/yeezysupply.service.ts
@@ -4,30 +4,59 @@ const { CookieJar } = require('tough-cookie');
 let got = require('got')
 import { AppConfig } from './../../../environments/environment'
 let db = require('diskdb')
+const unzipResponse = require('unzip-response');
 
 @Injectable({
   providedIn: 'root'
 })
 export class YeezySupplyService {
 
+  private cookieJar = new CookieJar()
   private task: any
   private mouseData: any = null
   private mouseDataDatabase: any
+  private validCookie: String
+  private userAgent: string
+
   constructor(
     private requestService: RequestService
   ) { }
 
   async make(task: any) {
-    console.log(task)
     this.task = task
-    const cookieJar = new CookieJar()
-    this.getCookie(cookieJar, true)
 
     if(this.task.product.mode == 'safe') {
       this.mouseDataDatabase = db.connect('/hornet/db', ['mousedata'])
       this.mouseData = this.mouseDataDatabase.mousedata.find()
     }
+    this.task.info.status = 'other|Getting Cookie'
+    await this.getCookie(this.cookieJar, true)
 
+    this.task.info.status = 'other|Finding product'
+    await this.checkProduct()
+  
+  }
+
+  async checkProduct() {
+    console.log(`https://www.yeezysupply.com/api/products/${this.task.product.id}/availability`)
+    let response = await got(`https://www.yeezysupply.com/api/products/${this.task.product.id}/availability`, {
+      decompress: false,
+      method: 'GET',
+      cookieJar: this.cookieJar,
+      gzip: true, 
+      headers: {
+        'user-agent': this.userAgent,
+        'origin': 'http://www.yeezysupply.com',
+				'referer': `https://www.yeezysupply.com/product/${this.task.product.id}/`,
+        'content-type': 'application/json',
+				'accept': 'application/json',
+				'accept-encoding': 'gzip, deflate',
+				'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin'
+      }
+    })
+    let res = unzipResponse(response)
+    console.log(res)
   }
 
   async getSensorData(cookie: any = false, mousedata: any = false) {
@@ -35,22 +64,21 @@ export class YeezySupplyService {
       decompress: false,
       headers: {
         'authorization': btoa(`${AppConfig.akamai.api.username}:${AppConfig.akamai.api.password}`),
-        'abck-cookie': cookie ? cookie : 'nothing', 
-        'mousedata': mousedata ? mousedata : 'nothing'
+        'abck-cookie': cookie ? cookie : 'false', 
+        'mousedata': mousedata ? mousedata : 'false'
       }
     })
     return JSON.parse(response.body)
   }
 
   async getCookie(jar: any, initial: Boolean = false) {
-    this.task.info.status = 'other|Getting Cookie'
     let sensorDataResponse: any, form: any, abck: any, validCookie: any
     if(initial) {
       sensorDataResponse = await this.getSensorData()
       form = {
         'sensor_data': sensorDataResponse.sensor_data
       }
-      // First request
+      this.userAgent = sensorDataResponse.user_agent
       let firstRequest = await got(`https://www.yeezysupply.com/static/${AppConfig.akamai.name}`, { 
         cookieJar: jar,
         method: "post",
@@ -70,14 +98,15 @@ export class YeezySupplyService {
 					abck = firstRequest.headers['set-cookie'][i].split(';')[0].replace('_abck=', '')
 				}
       }
+      
       let selectedMouseData = this.mouseData[0]
-      console.log(selectedMouseData.data)
       sensorDataResponse = await this.getSensorData(abck, JSON.stringify(selectedMouseData.data))
       this.mouseDataDatabase.mousedata.remove({_id: selectedMouseData._id})
+
       form = {
         'sensor_data': sensorDataResponse.sensor_data
       }
-      console.log(`https://www.yeezysupply.com/static/${AppConfig.akamai.name}`)
+
       let secondRequest = await got(`https://www.yeezysupply.com/static/${AppConfig.akamai.name}`, { 
         cookieJar: jar,
         method: "post",
@@ -88,7 +117,7 @@ export class YeezySupplyService {
           'referer': 'https://www.yeezysupply.com/',
           'user-agent': sensorDataResponse.user_agent,
           'content-type': 'text/plain',
-          'content-length': JSON.stringify(form).length
+          'content-length': form.length
         }
       })
 
@@ -98,10 +127,13 @@ export class YeezySupplyService {
         }
       }
 
-      console.log('asd', secondRequest)
-      console.log('valid', validCookie)
+      this.validCookie = validCookie
 
     }
+  }
+
+  async addToCart() {
+
   }
 
 
